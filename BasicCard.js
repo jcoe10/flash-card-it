27ca83c115d23074d97b029c0c3796372b3034a8
jcoe10 <jacob.e.coe@gmail.com>
2017-09-26 20:53:05 -0400

    added in an object with questions and answers

--- BasicCard.js
+++ BasicCard.js
@@ -1,10 +1,74 @@
+var inquirer = require('inquirer');
+
 //function for this.front and this.back
-function basicCard(front, back){
+function basicCard(front, back) {
     this.front = front;
     this.back = back;
 }
-//
+inquirer.prompt([
 
 
+    // var q1 = new basicCard("Who was the first president of the United States?", "George Washington");
 
-module.exports = BasicCard;
+    // // "Who was the first president of the United States?"
+    // console.log(q1.front); 
+
+    // // "George Washington"
+    // console.log(q1.back); 
+    {
+        message: "Who was the first president of the United States?",
+        name: 'George Washington'
+    },
+
+    // var q2 = new basicCard("Who was the first president of the United States?", "George Washington");
+
+    // // "Who was the first president of the United States?"
+    // console.log(q2.front); 
+
+    // // "George Washington"
+    // console.log(q2.back);
+    {
+        message: 'How many colonies was the United States originally?',
+        name: 'thirteen'
+    },
+
+    // var q3 = new basicCard("Who was the first president of the United States?", "George Washington");
+
+    // // "Who was the first president of the United States?"
+    // console.log(q3.front); 
+
+    // // "George Washington"
+    // console.log(q3.back);
+    {
+        message: "When was America Founded?",
+        name: 'July 4, 1776'
+    },
+
+    // var q4 = new basicCard("Who was the first president of the United States?", "George Washington");
+
+    // // "Who was the first president of the United States?"
+    // console.log(q4.front); 
+
+    // // "George Washington"
+    // console.log(q4.back);
+    {
+        message: "Which country did America buy the Louisiana Purchase from?",
+        name: 'France'
+    },
+
+    // var q5 = new basicCard("Who was the first president of the United States?", "George Washington");
+
+    // // "Who was the first president of the United States?"
+    // console.log(q5.front); 
+
+    // // "George Washington"
+    // console.log(q5.back); 
+    {
+        message: "Then deadliest war in American history is?",
+        name: 'The Civil War'
+    }
+
+
+]);
+
+// module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,12 @@
+//function for this.text and this.cloze
+function clozeCard(text, cloze){
+    this.text = text;
+    this.cloze = cloze;
+    this.partial = partial;
+    this.fullText = fullText;
+}
+
+clozeCard.prototype
+
+
+module.exports = ClozeCard;
