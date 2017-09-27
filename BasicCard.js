2e45f17d05093a498995c894da4cefdaac5e3db7
jcoe10 <jacob.e.coe@gmail.com>
2017-09-27 08:42:04 -0400

    added constructer set up for questions

--- BasicCard.js
+++ BasicCard.js
@@ -5,70 +5,49 @@ function basicCard(front, back) {
     this.front = front;
     this.back = back;
 }
-inquirer.prompt([
+
+var q1 = new basicCard("Who was the first president of the United States?", "George Washington");
+
+// "Who was the first president of the United States?"
+console.log(q1.front);
+
+// "George Washington"
+console.log(q1.back);
 
 
-    // var q1 = new basicCard("Who was the first president of the United States?", "George Washington");
+var q2 = new basicCard('How many colonies was the United States originally?', 'thirteen');
 
-    // // "Who was the first president of the United States?"
-    // console.log(q1.front); 
+// 'How many colonies was the United States originally?'
+console.log(q2.front);
 
-    // // "George Washington"
-    // console.log(q1.back); 
-    {
-        message: "Who was the first president of the United States?",
-        name: 'George Washington'
-    },
-
-    // var q2 = new basicCard("Who was the first president of the United States?", "George Washington");
-
-    // // "Who was the first president of the United States?"
-    // console.log(q2.front); 
-
-    // // "George Washington"
-    // console.log(q2.back);
-    {
-        message: 'How many colonies was the United States originally?',
-        name: 'thirteen'
-    },
-
-    // var q3 = new basicCard("Who was the first president of the United States?", "George Washington");
-
-    // // "Who was the first president of the United States?"
-    // console.log(q3.front); 
-
-    // // "George Washington"
-    // console.log(q3.back);
-    {
-        message: "When was America Founded?",
-        name: 'July 4, 1776'
-    },
-
-    // var q4 = new basicCard("Who was the first president of the United States?", "George Washington");
-
-    // // "Who was the first president of the United States?"
-    // console.log(q4.front); 
-
-    // // "George Washington"
-    // console.log(q4.back);
-    {
-        message: "Which country did America buy the Louisiana Purchase from?",
-        name: 'France'
-    },
-
-    // var q5 = new basicCard("Who was the first president of the United States?", "George Washington");
-
-    // // "Who was the first president of the United States?"
-    // console.log(q5.front); 
-
-    // // "George Washington"
-    // console.log(q5.back); 
-    {
-        message: "Then deadliest war in American history is?",
-        name: 'The Civil War'
-    }
+// 'thirteen'
+console.log(q2.back);
 
 
-]);
+var q3 = new basicCard("When was America Founded?", 'July 4, 1776');
 
-// module.exports = BasicCard;
+// "When was America Founded?"
+console.log(q3.front);
+
+// 'July 4, 1776'
+console.log(q3.back);
+
+
+var q4 = new basicCard("Which country did America buy the Louisiana Purchase from?", 'France');
+
+// "Which country did America buy the Louisiana Purchase from?"
+console.log(q4.front);
+
+// 'France'
+console.log(q4.back);
+
+
+var q5 = new basicCard("Then deadliest war in American history is?", 'The Civil War');
+
+// "Then deadliest war in American history is?"
+console.log(q5.front);
+
+// 'The Civil War'
+console.log(q5.back);
+
+module.exports = basicCard;
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,12 +1,95 @@
 //function for this.text and this.cloze
-function clozeCard(text, cloze){
+function clozeCard(text, cloze, partial, fullText){
     this.text = text;
     this.cloze = cloze;
     this.partial = partial;
     this.fullText = fullText;
 }
 
-clozeCard.prototype
+//Question 1
+var q1 = new clozeCard("George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(q1.cloze); 
+
+// " ... was the first president of the United States.
+console.log(q1.partial);
+
+// "George Washington was the first president of the United States.
+console.log(q1.fullText);
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new clozeCard("Error", "Invalid Entry");
 
 
-module.exports = ClozeCard;
+//Question 2
+var q2 = new clozeCard("There were thirteen colonies the United States.", "thirteen");
+
+// "George Washington"
+console.log(q2.cloze); 
+
+// There were ... colonies the United States.".
+console.log(q2.partial);
+
+// "There were thirteen colonies the United States."
+console.log(q2.fullText);
+
+// Should throw or log an error 
+var brokenCloze = new clozeCard("Error", "Invalid Entry");
+
+
+
+
+//Question 3
+var q3 = new clozeCard("America was founded on July 4, 1176.", "July 4, 1776");
+
+// "July 4, 1776"
+console.log(q3.cloze); 
+
+// "America was founded on ... ."
+consoles.log(q3.partial);
+
+// "America was founded on July 4, 1176."
+console.log(q3.fullText);
+
+// Should throw or log an error
+var brokenCloze = new clozeCard("Error", "Invalid Entry");
+
+
+
+
+// //Question 4
+// var q4 = new clozeCard("America bought the Louisiana Purchase from France.", "France");
+
+// // "France"
+// console.log(q4.cloze); 
+
+// // "America bought the Louisiana Purchase from ... ."
+// console.log(q4.partial);
+
+// // "America bought the Louisiana Purchase from France."
+// console.log(q4.fullText);
+
+// // Should throw or log an error 
+// var brokenCloze = new clozeCard("Error", "Invalid Entry");
+
+
+
+
+
+// //Question 5
+// var q5 = new clozeCard("The Civil War was the deadliest war in American history.", "The Civil War");
+
+// // "The Civil War"
+// console.log(q5.cloze); 
+
+// // " ... was the deadliest war in American history.""
+// console.log(q5.partial);
+
+// // "The Civil War was the deadliest war in American history."
+// console.log(q5.fullText);
+
+// // Should throw or log an error
+// var brokenCloze = new clozeCard("Error", "Invalid Entry");
+
+module.exports = clozeCard;
